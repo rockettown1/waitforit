4f0feee6ba02ee945ac95d76aff0569f3a4c6e63
Dan Krishnan <dan@wearecodenation.com>
2020-04-19 00:28:27 +0100

    Updates

---
+++ precache-manifest.d4938cd42e2517ab0dd8af131ea1fea6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d29e38107b2523611a96e28cc972def5",
+    "url": "/waitforit/index.html"
+  },
+  {
+    "revision": "9fa084ba9b9ec4c80302",
+    "url": "/waitforit/static/css/2.a33d7f97.chunk.css"
+  },
+  {
+    "revision": "dafc96d8088c01b5ad12",
+    "url": "/waitforit/static/css/main.f9f8f5f7.chunk.css"
+  },
+  {
+    "revision": "9fa084ba9b9ec4c80302",
+    "url": "/waitforit/static/js/2.561b4996.chunk.js"
+  },
+  {
+    "revision": "8a9adb48f7af2062183ac70b4922c4ef",
+    "url": "/waitforit/static/js/2.561b4996.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dafc96d8088c01b5ad12",
+    "url": "/waitforit/static/js/main.32e557ee.chunk.js"
+  },
+  {
+    "revision": "f42858311dce4da71960",
+    "url": "/waitforit/static/js/runtime-main.86925709.js"
+  },
+  {
+    "revision": "21daf0eb5c6c7c11ae1dde001060934d",
+    "url": "/waitforit/static/media/Codenation.21daf0eb.png"
+  },
+  {
+    "revision": "f1b11487efba071ccb5531231fd7a673",
+    "url": "/waitforit/static/media/Leon.f1b11487.jpg"
+  },
+  {
+    "revision": "fc2e191c9c4bf931adcf022ee32819b9",
+    "url": "/waitforit/static/media/wearecodenation.fc2e191c.png"
+  }
+]);
